d328367bd71475eedcae71761d3a80a97113f818
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-04-04 15:31:39 -0700

    Added btree index

--- commands/Index.java
+++ commands/Index.java
@@ -1,14 +1,16 @@
 package commands;
 
+import bufmgr.*;
 import columnar.ColumnarFile;
 import global.SystemDefs;
 
-import static global.GlobalConst.NUMBUF;
+import java.io.IOException;
 
 public class Index {
 
     public static void main(String [] command) {
 
+//        command = "testdb test1 A BTREE".split(" ");
         if (command.length != 4) {
             System.out.println("Error: Incorrect number of input values.");
             return;
@@ -19,11 +21,9 @@ public class Index {
         String columnName = command[2];
         String indexType = command[3];
 
-        String dbpath = "/tmp/"+columnDBName+".minibase-db";
-        String logpath = "/tmp/"+columnDBName+".minibase-log";
+        String dbpath = "/tmp/"+ columnDBName;
 
         SystemDefs sysdef = new SystemDefs( dbpath, 0, 300, "Clock" );
-
 
         ColumnarFile columnarFile = new ColumnarFile(columnarFileName);
         if(indexType.equals("BITMAP")) {
@@ -33,6 +33,13 @@ public class Index {
             columnarFile.createBtreeIndex(columnarFile.getColumnNo(columnName));
         } else {
             System.out.println("Error: Incorrect index type.");
+        }
+
+        try {
+            SystemDefs.JavabaseBM.flushAllPages();
+        } catch (HashOperationException | PageUnpinnedException | PagePinnedException | PageNotFoundException |
+                 BufMgrException | IOException e) {
+            throw new RuntimeException("Error flushing pages",e);
         }
 
         System.out.println(indexType + " Index created on column " + columnName);
