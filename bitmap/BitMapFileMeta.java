bbed1cf68af08d104866e292adb037f8f36afd57
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-04-05 16:04:07 -0700

    dont need to save columnNo

--- bitmap/BitMapFileMeta.java
+++ bitmap/BitMapFileMeta.java
@@ -11,62 +11,48 @@ import heap.Tuple;
 import java.io.IOException;
 
 public class BitMapFileMeta {
-    int columnNo;
     String bitMapFileName;
     String value;
 
-    public BitMapFileMeta(int columnNo, String bitMapFileName, String value) {
-        this.columnNo = columnNo;
+    private final static AttrType[] attrTypes = new AttrType[] {
+            new AttrType(AttrType.attrString), // bitmapfileName
+            new AttrType(AttrType.attrString), // value
+    };
+
+    public BitMapFileMeta(String bitMapFileName, String value) {
         this.bitMapFileName = bitMapFileName;
         this.value = value;
     }
 
-    public BitMapFileMeta(byte[] data) throws FieldNumberOutOfBoundException, IOException {
-        Tuple tuple = new Tuple(data);
-        this.columnNo = tuple.getIntFld(1);
-        this.bitMapFileName = tuple.getStrFld(2);
-        this.value = tuple.getStrFld(3);
+    public BitMapFileMeta(byte[] data) {
+        try{
+            Tuple tuple = new Tuple(data);
+            this.bitMapFileName = tuple.getStrFld(1);
+            this.value = tuple.getStrFld(2);
+        } catch (FieldNumberOutOfBoundException | IOException e) {
+            throw new RuntimeException("Error getting data from tuple",e);
+        }
+
     }
 
     public String getName() {
         return bitMapFileName;
     }
 
-    public ValueClass getValue(int attrType) {
-        switch (attrType) {
-            case AttrType.attrString: {
-                return new ValueString(value);
-            }
-
-            case AttrType.attrInteger: {
-                return new ValueInt(Integer.parseInt(value));
-            }
-
-            case AttrType.attrReal: {
-                return new ValueFloat(Float.parseFloat(value));
-            }
-
-            default: {
-                throw  new RuntimeException("Attr not found");
-            }
-        }
-    }
-
     public Tuple convertToTuple() {
         Tuple tuple = new Tuple();
         try {
-            tuple.setHdr((short)3, new AttrType[] {
-                new AttrType(AttrType.attrInteger), // Column No
-                new AttrType(AttrType.attrString), // bitmapfileName
-                new AttrType(AttrType.attrString), // value
-            }, new short[]{(short)bitMapFileName.length(), (short)value.length()});
-            tuple.setIntFld(1, columnNo);
-            tuple.setStrFld(2, bitMapFileName);
-            tuple.setStrFld(3, value);
+            tuple.setHdr((short)2, attrTypes , new short[]{(short)bitMapFileName.length(), (short)value.length()});
+            tuple.setStrFld(1, bitMapFileName);
+            tuple.setStrFld(2, value);
         }
         catch (Exception e) {
             throw new RuntimeException(e);
         }
         return tuple;
+    }
+
+    public static AttrType[] getAttrTypes() {
+        return attrTypes;
     }
 }
