4f91ea3f60dd1791cba5a08823daa5f252e1127e
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-04-15 23:27:59 -0700

    updated getValue

--- bitmap/BitMapFileMeta.java
+++ bitmap/BitMapFileMeta.java
@@ -1,6 +1,9 @@
 package bitmap;
 
+import columnar.ValueInt;
+import columnar.ValueString;
 import global.AttrType;
+import global.ValueClass;
 import heap.FieldNumberOutOfBoundException;
 import heap.Tuple;
 
@@ -10,14 +13,18 @@ public class BitMapFileMeta {
     String bitMapFileName;
     String value;
 
+    AttrType attrType;
+
     private final static AttrType[] attrTypes = new AttrType[] {
             new AttrType(AttrType.attrString), // bitmapfileName
             new AttrType(AttrType.attrString), // value
+            new AttrType(AttrType.attrInteger)
     };
 
-    public BitMapFileMeta(String bitMapFileName, String value) {
+    public BitMapFileMeta(String bitMapFileName, String value, AttrType attrType) {
         this.bitMapFileName = bitMapFileName;
         this.value = value;
+        this.attrType = attrType;
     }
 
     public BitMapFileMeta(byte[] data) {
@@ -25,6 +32,7 @@ public class BitMapFileMeta {
             Tuple tuple = new Tuple(data);
             this.bitMapFileName = tuple.getStrFld(1);
             this.value = tuple.getStrFld(2);
+            this.attrType = new AttrType(tuple.getIntFld(3));
         } catch (FieldNumberOutOfBoundException | IOException e) {
             throw new RuntimeException("Error getting data from tuple",e);
         }
@@ -38,9 +46,10 @@ public class BitMapFileMeta {
     public Tuple convertToTuple() {
         Tuple tuple = new Tuple();
         try {
-            tuple.setHdr((short)2, attrTypes , new short[]{(short)bitMapFileName.length(), (short)value.length()});
+            tuple.setHdr((short)3, attrTypes , new short[]{(short)bitMapFileName.length(), (short)value.length()});
             tuple.setStrFld(1, bitMapFileName);
             tuple.setStrFld(2, value);
+            tuple.setIntFld(3,attrType.attrType);
         }
         catch (Exception e) {
             throw new RuntimeException(e);
@@ -48,11 +57,20 @@ public class BitMapFileMeta {
         return tuple;
     }
 
-    public static AttrType[] getAttrTypes() {
-        return attrTypes;
-    }
+    public ValueClass getValue() {
 
-    public String getValue() {
-        return value;
+        switch (attrType.attrType) {
+            case AttrType.attrInteger -> {
+                return new ValueInt(Integer.parseInt(value));
+            }
+
+            case AttrType.attrString -> {
+                return new ValueString(value);
+            }
+
+            default -> {
+                throw new IllegalArgumentException("Attribute error");
+            }
+        }
     }
 }
