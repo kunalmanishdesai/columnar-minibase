a77862663191518781d5ae335959951a0b3ee078
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-04-21 21:27:14 -0700

    Added time

--- diskmgr/PCounter.java
+++ diskmgr/PCounter.java
@@ -3,10 +3,13 @@ package diskmgr;
 public class PCounter {
     public static int rcounter;
     public static int wcounter;
+
+    public static long startTime;
     //initialize is called when the DB is first initialized
     public static void initialize() {
         rcounter =0;
         wcounter =0;
+        startTime = System.nanoTime();
     }
     //readcounter is incremented
     public static void readIncrement() {
@@ -17,8 +20,13 @@ public class PCounter {
         wcounter++;
     }
 
+
+
     public static void print() {
         System.out.println("No. of pages read " +rcounter);
         System.out.println("No. of pages written " +wcounter);
+
+        long executionTime = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Time taken: " +executionTime + " milliseconds");
     }
 }
